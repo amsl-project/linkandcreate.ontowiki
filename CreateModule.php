de6577adaf972ebb2c3ce23b7f8f1bb1256bbaa0
Reik Mueller <reik.mueller@uni-leipzig.de>
2016-04-15 12:46:07 +0200

    FIX: empty suggestion list

--- CreateModule.php
+++ CreateModule.php
@@ -68,12 +68,102 @@ class CreateModule extends OntoWiki_Module
         $event = new Erfurt_Event('onResourceShowRanges');
         $event->resource = $selectedResource;
         $event->hideProperties = $this->_hideProperties;
-        $event->trigger();
 
-        $data['linkData'] = $event->data;
+        $data['linkData'] = $this->showRanges($event);
 
         require_once('LinkandcreateController.php');
         return $this->render('linkandcreate/linkandcreate', $data);
+    }
+
+    public function showRanges($event)
+    {
+        $owApp = OntoWiki::getInstance();
+        $this->_model = $owApp->selectedModel;
+        $resourceObject = $event->resource;
+        $rModel = $resourceObject->getMemoryModel();
+        $titleHelper = new OntoWiki_Model_TitleHelper();
+        $hideProperties = $event->hideProperties;
+
+        $temp = array();
+
+        foreach ($hideProperties as $name) {
+            $temp[$name['propertyUri']] = '';
+        }
+
+        $hideProperties = $temp;
+
+        $values = $rModel->getValues($resourceObject->getUri(), EF_RDF_TYPE);
+        $data = array();
+
+        if (count($values) === 1) {
+            $class = $values[0]['value'];
+        } else {
+            return $data;
+        }
+
+        $query = 'SELECT DISTINCT ?p ?range ?oneOf WHERE ' . PHP_EOL;
+        $query.= ' ' . PHP_EOL;
+        $query.= '{ ' . PHP_EOL;
+        $query.= '  ?s a <' . $class . '> . ' . PHP_EOL;
+        $query.= '  ?s ?p ?o . ' . PHP_EOL;
+        $query.= '  ?p <' . EF_RDFS_RANGE . '> ?range . ' . PHP_EOL;
+        $query.= '  OPTIONAL ' . PHP_EOL;
+        $query.= '  { ' . PHP_EOL;
+        $query.= '      ?range <' . EF_OWL_ONEOF . '> ?oneOf . ' . PHP_EOL;
+        $query.= '  } ' . PHP_EOL;
+        $query.= '} ' . PHP_EOL;
+
+        $results = $this->_model->sparqlQuery($query);
+
+        if (count($results) === 0) {
+            return $data;
+        }
+
+        foreach ($results as $result) {
+
+            if (strpos($result['range'], 'XMLSchema#') === false
+                && (!isset($result['oneOf']) || $result['oneOf'] === '' || is_null($result['oneOf']))
+                && $result['p'] !== EF_RDF_TYPE
+                && !isset($hideProperties[$result['p']])
+            ) {
+                $data[] = array(
+                    'property' => $result['p'],
+                    'propertyLabel' => $titleHelper->getTitle($result['p']),
+                    'class' => $result['range'],
+                    'classLabel' => $titleHelper->getTitle($result['range'])
+                );
+            }
+        }
+
+        $delete = array();
+
+        foreach($data as $key => $range) {
+            $query  = 'SELECT ?testCollection WHERE ';
+            $query .= '{ ' . PHP_EOL;
+            $query .= '  <' . $range['class'] . '> ?p ?testCollection . ' . PHP_EOL;
+            $query .= '  ?testCollection <' . EF_RDF_FIRST . '> ?test . ' . PHP_EOL;
+            $query .= '} ' . PHP_EOL;
+            $ranges = $this->_model->sparqlQuery($query);
+
+            if (count($ranges) > 0) {
+                $delete[] = $key;
+                $this->_getCollection($ranges[0]['testCollection']);
+                foreach ($this->_ranges as $foundRange) {
+                    $data[] = array(
+                        'property' => $range['property'],
+                        'propertyLabel' => $titleHelper->getTitle($range['property']),
+                        'class' => $foundRange,
+                        'classLabel' => $titleHelper->getTitle($foundRange)
+                    );
+                }
+            }
+        }
+
+        foreach ($delete as $key) {
+            unset($data[$key]);
+        }
+
+        return $data;
     }
 
     /*
@@ -96,11 +186,11 @@ class CreateModule extends OntoWiki_Module
                 continue;
             }
             if (
-                $rModel->hasSPvalue(
-                    (string) $resource,
-                    EF_RDF_TYPE,
-                    $classUri
-                )
+            $rModel->hasSPvalue(
+                (string) $resource,
+                EF_RDF_TYPE,
+                $classUri
+            )
             ) {
                 return true;
             }
@@ -110,5 +200,4 @@ class CreateModule extends OntoWiki_Module
         return false;
     }
 }
-
 
